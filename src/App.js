c61257a41695761cb664ba58c13d099ddb7ccd4a
raoyashwant <raoyashwant132@gmail.com>
2024-02-18 01:25:27 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import React from "react";
-import VR_IMAGE from './vr.jpeg'
-import { Pannellum, PannellumVideo } from "pannellum-react";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-        <Pannellum
-            width="100%"
-            height="500px"
-            image={VR_IMAGE}
-            yaw={180}
-            hfov={110}
-            autoLoad
-        >
-          {/* <Pannellum.Hotspot
-            type="info"
-            pitch={11}
-            yaw={-167}
-            text="Info Hotspot Text 3"
-            URL="https://github.com/farminf/pannellum-react"
-          /> */}
-        </Pannellum>
+    <div className="App">
+      <header className="App-header">
+        HELLO
+      </header>
     </div>
-  )
+  );
 }
 
 export default App;
