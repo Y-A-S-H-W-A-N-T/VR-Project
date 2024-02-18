efbe59244b7412affd8f074fb40a30d8bad37acf
raoyashwant <raoyashwant132@gmail.com>
2024-02-19 00:01:58 +0530

    plz work

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
+import React from 'react'
 import VR_IMAGE from './vr.jpeg'
+
 import { Pannellum, PannellumVideo } from "pannellum-react";
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Pannellum
-            width="100%"
-            height="500px"
-            image={VR_IMAGE}
-            yaw={180}
-            hfov={110}
-            autoLoad
-        >
-        </Pannellum>
+        width="100%"
+        height="500px"
+        image={VR_IMAGE}
+        pitch={10}
+        yaw={180}
+        hfov={110}
+        autoLoad
+    >
+    </Pannellum>
+
+    {/* <PannellumVideo
+      video={myVideo}
+      loop
+      width="100%"
+      height="600px"
+      pitch={10}
+      yaw={180}
+      hfov={140}
+      minHfov={50}
+      maxHfov={180}
+    >
+      <Pannellum.Hotspot
+        type="custom"
+        pitch={31}
+        yaw={150}
+        handleClick={(evt , name) => this.hanldeClick(name)}
+        name="hs1"
+      />
+
+      <Pannellum.Hotspot
+        type="info"
+        pitch={31}
+        yaw={-57}
+        text="Info"
+        URL="https://github.com/farminf"
+      />
+    </PannellumVideo> */}
+
     </div>
   );
 }
