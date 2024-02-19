52cb4ef4c9544cdc8d8df8024e38750cf2afd140
raoyashwant <raoyashwant132@gmail.com>
2024-02-19 22:15:30 +0530

    trying vr cardboard

--- src/App.js
+++ src/App.js
@@ -1,50 +1,36 @@
 import React from 'react'
 import VR_IMAGE from './vr.jpeg'
 
-import { Pannellum, PannellumVideo } from "pannellum-react";
+import { Pannellum, PannellumVideo } from "pannellum-react"
+import './css/panoramic.css'
 
 function App() {
   return (
     <div className="App">
-      <Pannellum
-        width="100%"
-        height="500px"
-        image={VR_IMAGE}
-        pitch={10}
-        yaw={180}
-        hfov={110}
-        autoLoad
-    >
-    </Pannellum>
-
-    {/* <PannellumVideo
-      video={myVideo}
-      loop
-      width="100%"
-      height="600px"
-      pitch={10}
-      yaw={180}
-      hfov={140}
-      minHfov={50}
-      maxHfov={180}
-    >
-      <Pannellum.Hotspot
-        type="custom"
-        pitch={31}
-        yaw={150}
-        handleClick={(evt , name) => this.hanldeClick(name)}
-        name="hs1"
-      />
-
-      <Pannellum.Hotspot
-        type="info"
-        pitch={31}
-        yaw={-57}
-        text="Info"
-        URL="https://github.com/farminf"
-      />
-    </PannellumVideo> */}
-
+      <div className='vr'>
+        <div className='pano'>
+          <Pannellum
+            image={VR_IMAGE}
+            pitch={10}
+            yaw={180}
+            hfov={110}
+            autoLoad
+            autoRotate
+          >
+          </Pannellum>
+        </div>
+        <div className='pano'>
+          <Pannellum
+            image={VR_IMAGE}
+            pitch={10}
+            yaw={180}
+            hfov={110}
+            autoLoad
+            autoRotate
+          >
+          </Pannellum>
+        </div>
+      </div>
     </div>
   );
 }
