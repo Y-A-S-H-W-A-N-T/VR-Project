1e2743369ca2172ee708f9e58bb9cc09d97db54b
raoyashwant <raoyashwant132@gmail.com>
2024-02-20 23:29:44 +0530

    trying aframe

--- src/App.js
+++ src/App.js
@@ -4,33 +4,30 @@ import VR_IMAGE from './vr2.jpg'
 import { Pannellum, PannellumVideo } from "pannellum-react"
 import './css/panoramic.css'
 
+import 'aframe';
+import 'aframe-particle-system-component';
+import {Entity, Scene, Sky} from 'aframe-react';
+
 function App() {
   return (
     <div className="App">
-        <div className='pano'>
-          <Pannellum
-            image={VR_IMAGE}
-            height='300px'
-            pitch={10}
-            yaw={90}
-            hfov={110}
-            autoLoad
-            orientationOnByDefault
-          >
-          </Pannellum>
-        </div>
-        <div className='pano'>
-          <Pannellum
-            image={VR_IMAGE}
-            height='300px'
-            pitch={10}
-            yaw={90}
-            hfov={110}
-            autoLoad
-            orientationOnByDefault
-          >
-          </Pannellum>
-        </div>
+      {/* <div className='pano'>
+        <Pannellum
+          image={VR_IMAGE}
+          height='300px'
+          pitch={10}
+          yaw={90}
+          hfov={110}
+          autoLoad
+          orientationOnByDefault
+        >
+        </Pannellum>
+      </div> */}
+      <Scene xr-mode-ui="enabled: false">
+        <a-Sky src={VR_IMAGE} rotation='0 -130 0'/>
+        <a-Camera cursor-visible="false"></a-Camera>
+      </Scene>
+      
       </div>
   );
 }
