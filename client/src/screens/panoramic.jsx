73b12969fbf3dd8562a16be19b796d9a23346a20
raoyashwant <raoyashwant132@gmail.com>
2024-02-22 22:58:07 +0530

    test

--- client/src/screens/panoramic.jsx
+++ client/src/screens/panoramic.jsx
@@ -2,32 +2,15 @@ import React from 'react'
 import '../css/panoramic.css'
 import VR_IMAGE from '../vr.jpeg'
 import { Pannellum, PannellumVideo } from 'pannellum-react'
+import 'aframe'
+import { Entity, Scene } from 'aframe-react'
 
 function Panoramic() {
   return (
     <div>
-      <div className='stereoscopic-view'>
-        <div className='pano-screen'>
-          <Pannellum
-            image={VR_IMAGE}
-            pitch={10}
-            yaw={90}
-            hfov={110}
-            autoLoad
-            orientationOnByDefault
-          />
-        </div>
-        <div className='pano-screen'>
-          <Pannellum
-            image={VR_IMAGE}
-            pitch={10}
-            yaw={90}
-            hfov={110}
-            autoLoad
-            orientationOnByDefault
-          />
-        </div>
-        </div>
+      <Scene>
+          <a-sky src={VR_IMAGE}/>
+      </Scene>
     </div>
   )
 }
