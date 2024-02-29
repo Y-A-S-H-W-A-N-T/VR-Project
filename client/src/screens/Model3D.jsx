39f89537a64bf8cf43adfced53cced73c9b9b17d
raoyashwant <raoyashwant132@gmail.com>
2024-03-01 02:58:58 +0530

    Ar added for PC

--- client/src/screens/Model3D.jsx
+++ client/src/screens/Model3D.jsx
@@ -2,18 +2,33 @@ import React from 'react'
 import 'aframe'
 import 'aframe-particle-system-component';
 import Table from '../table.png'
+import Tv from '../vr1.jpg'
 import { useLocation, Link } from 'react-router-dom'
+import { useState, useEffect } from 'react';
 
 function Model3D() {
 
   const location = useLocation()
-  const { pano } = location.state
+  const { pano, furniture } = location.state
+  var temp = furniture
+
+  const [Furniture,setFurniture] = useState(null)
+  
+  useEffect(()=>{
+    async function showFurniture(temp){
+      if(furniture=='tv')
+        setFurniture(Tv)
+      if(furniture=='table')
+        setFurniture(Table)
+    }
+    showFurniture()
+  },[])
 
   return (
     <div>
       <a-scene>
         <a-sky src={pano}></a-sky>
-        <a-image src={Table} position="0 0 -5" height="0.9" width="0.9"></a-image>
+        <a-image src={`${Furniture}`} position="0 0 -5" height="0.9" width="0.9"></a-image>
       </a-scene>
     </div>
   )
