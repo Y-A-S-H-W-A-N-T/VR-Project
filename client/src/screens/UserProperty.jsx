315b13a208d3a23fd44e392265c92aa68e991b4d
SOUMYAPRATAPSINGH <120254048+SOUMYAPRATAPSINGH@users.noreply.github.com>
2024-03-11 13:34:46 +0530

    share button

--- client/src/screens/UserProperty.jsx
+++ client/src/screens/UserProperty.jsx
@@ -202,7 +202,7 @@ function UserPropertyList() {
       className="block w-full select-none rounded-lg bg-gray-900 py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
       type="button"
       onClick={(e) => Share(e, item._id)}>
-      Reserve
+      Share
     </button>
   </div>
                 ) : (
