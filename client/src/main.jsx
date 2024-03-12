c56b7c2b29f98a5b30eaa7b2d38c4389082e0c29
AwishChhetri <abishchhetr.2502@gamil.com>
2024-03-13 01:14:41 +0530

    deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,8 +6,8 @@ import axios from 'axios'
 import { UserProvider } from './useContext.jsx'
 import { ToastContainer  } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-// axios.defaults.baseURL="https://vr-project-hzru.onrender.com"
-axios.defaults.baseURL="http://localhost:3000"
+ axios.defaults.baseURL="https://vr-project-hzru.onrender.com"
+// axios.defaults.baseURL="http://localhost:3000"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
